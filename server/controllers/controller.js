5b5e5b66fb13add430b99ed1d26246691cf30abd
Shrikant Vishwakarma <64365738+StrixV54@users.noreply.github.com>
2023-09-09 14:26:46 +0530

    fix optimization controller

--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -14,22 +14,23 @@ export const putDescription = async (req, res) => {
     const { id, description } = req.body;
     const movies = await readValue();
     const jsonMovies = JSON.parse(movies);
+    let isSame = false;
     const newData = jsonMovies.movies.map((item) => {
       if (item.id.toString() === id.toString()) {
         res.status(200);
         if (item.description === description) {
-          res.json({ message: "Value to be updated is same as before, hence not updating it." });
-          return;
+          isSame = true;
         } else {
           item.description = description;
         }
       }
       return item;
     });
-    if (res.statusCode === 200) {
+    if (isSame) res.json({ message: "Value to be updated is same as before, hence not updating it." });
+    else if (res.statusCode === 200 && !isSame) {
       writeValue(newData);
-      res.json({ message: "Success", newData });}
-    else
+      res.json({ message: "Successfully updated", newData });
+    } else
       res.status(404).json({ message: `Id: "${id}" not found in database.` });
   } catch (error) {
     res.status(500).json({ message: error.message });
